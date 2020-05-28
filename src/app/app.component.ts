8c52081a8aeddf827869dfabb4a6461b13c958ec
subhrajitbcrec <subhrajitbcrec@gmail.com>
2020-05-28 13:27:43 -0400

    Post impl

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,23 +1,46 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Greet } from './app.model.greet';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
 
-  title = 'HTTPDemo';
+  title = 'Welcome';
 
   constructor(private http: HttpClient) { }
 
   ngOnInit() {
-    this.http.get<any>('http://localhost:8080/greet').subscribe(data => {
+    //Make ServiceCall -> update Title -> then display the title
+    const headersVal = { userID: "Subhrajit" };
+    this.http.get<any>('http://localhost:8080/greet', { headers: headersVal }).subscribe(data => {
       console.log(data);
       this.title = data.content;
-    })
+    });
   }
 
 
+  submitName(nameVal) {
+    console.log(nameVal);
+    const body = new Greet(2, nameVal);
+    let resp = null;
+    var status = "running";
+    this.http.post('http://localhost:8080/addGreeting', body).subscribe({
+      next: data => {
+        console.log("Success", data);
+        status = "success";
+        resp = data;
+        this.title = resp.content;
+      },
+      error: error => {
+        console.error('There was an error!', error);
+        status = "failed";
+        this.title ='There was an error!';
+      }
+    })
+
+  }
 }
---
+++ src/app/app.model.greet.ts
@@ -0,0 +1,10 @@
+export class Greet{
+
+	id: number;
+	content : string;
+	
+	constructor(id, content){
+		this.id = id;
+		this.content = content;
+	}
+}
